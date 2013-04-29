5cf6677681c75d10c17821a5d914ebba87176514
Ray Rogers <ray@rayonrails.com>
2013-04-29 18:06:52 -0400

    base application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Risk::Application.config.secret_token = 'a984b3326d18409aaca013ed1f12ed9e6c35279fc214979e5b4b6b48562f9fbcba721ddc01ebe88938ce38a1164ad62db088ed245f78118c7cbf03dd72af9f0c'
